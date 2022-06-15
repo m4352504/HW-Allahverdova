586cc417eea69648092b38b1a0cf6842e0f56eef
m4352504 <m4352504@mail.ru>
2022-06-15 15:36:08 +0300

    Задача 4. Решение

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 какое меньшее.
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3 */
+a = -9 b = -3 -> max = -3 
 
 Console.WriteLine("Введите первое число");
 int a = int.Parse(Console.ReadLine());
@@ -20,19 +20,41 @@ else if(b > a)
 else
 {
     Console.WriteLine("Числа равны");
-}
+} */
 
 /* Задача 4: Напишите программу, которая принимает на
 вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
-22 3 9 -> 22
-Задача 6: Напишите программу, которая на вход
+22 3 9 -> 22*/
+
+Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+if (a >= b & a >= c)
+{
+    Console.WriteLine("Max = " + a);
+}
+else if (b >= a & b >= c)
+{
+    Console.WriteLine("Max = " + b);
+}
+else if (c >= b & c >= a)
+{
+    Console.WriteLine("Max = " + c);
+}
+
+/* Задача 6: Напишите программу, которая на вход
 принимает число и выдаёт, является ли число чётным
 (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет */
+
+
 
 /* Задача 8: Напишите программу, которая на вход
 принимает число (N), а на выходе показывает все чётные
